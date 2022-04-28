0673f2efcd0e2bb56c417c4de0e3e788ab78d28c
Gabrielly Moura <gabriellymouta@gmail.com>
2022-04-27 22:25:58 -0300

    finalizacao do carrosel do card cats Co-authored-by: Glacy Gomes <glacy.kelly3@gmail.com>

--- src/app/components/cats/cats.component.ts
+++ src/app/components/cats/cats.component.ts
@@ -4,45 +4,47 @@ import { GatinhosService } from 'src/app/shared/gatinhos.service';
 @Component({
   selector: 'app-cats',
   templateUrl: './cats.component.html',
-  styleUrls: ['./cats.component.css']
+  styleUrls: ['./cats.component.css'],
 })
 export class CatsComponent implements OnInit {
   gatos: any;
 
-  constructor(private gatinhosService: GatinhosService) { }
+  constructor(private gatinhosService: GatinhosService) {}
 
   ngOnInit(): void {
-    this.gatinhosService.getCatsBreed()
-    .subscribe(
-        (dados) => {
-          this.gatos = dados;
-        }
-    )
+    this.gatinhosService.getCatsBreed().subscribe((dados) => {
+      this.gatos = dados;
+    });
   }
 
   controlScrollLeft() {
     var container = document.getElementById('container');
-    this.sideScroll(container,'left',40,300,10);
+    this.sideScroll(container, 'left', 40, 260, 10);
   }
 
   controlScrollRight() {
     var container = document.getElementById('container');
-    this.sideScroll(container,'right',40,300,10);
+    this.sideScroll(container, 'right', 40, 260, 10);
   }
 
-  sideScroll(element:any,direction:string,speed:number,distance:number,step:number){
+  sideScroll(
+    element: any,
+    direction: string,
+    speed: number,
+    distance: number,
+    step: number
+  ) {
     let scrollAmount = 0;
-    var slideTimer = setInterval(function(){
-        if(direction == 'left'){
-            element.scrollLeft -= step;
-        } else {
-            element.scrollLeft += step;
-        }
-        scrollAmount += step;
-        if(scrollAmount >= distance){
-            window.clearInterval(slideTimer);
-        }
+    var slideTimer = setInterval(function () {
+      if (direction == 'left') {
+        element.scrollLeft -= step;
+      } else {
+        element.scrollLeft += step;
+      }
+      scrollAmount += step;
+      if (scrollAmount >= distance) {
+        window.clearInterval(slideTimer);
+      }
     }, speed);
   }
-
 }
